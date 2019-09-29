b7217e10a756746c9c9a881136a17e501ea84448
WangHao <897878405@QQ.COM>
2019-09-29 18:57:12 +0800

    评论页面
    评论vue组件+element UI分页/button

---
+++ commentlist/src/components/commentList.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <p>ID: {{content.id}} 时间:{{content.time}}</p>
+ 
+    <p class="comment">{{content .comment}}</p>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "commentList",
+  props: ["content"],
+  methods: {
+    
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+li{
+  list-style: none;
+
+}
+.comment{
+  background: #aaa;
+  border-radius: 5px;
+  padding:2px 5px;
+}
+</style>
